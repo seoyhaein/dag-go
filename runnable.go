459fd5d2d6a9b4bf52f85e187007d1fcfd35f73d
seoyhaein <seoy@bio-caleb.com>
2022-08-24 17:56:41 +0900

    commit by seoy.

--- runnable.go
+++ runnable.go
@@ -3,5 +3,5 @@ package dag_go
 type Runnable interface {
 	RunE(a interface{}) (int, error)
 	CreateImage(a interface{}, healthChecker string) error
-	Execute() (r int, err error)
+	//Execute() (r int, err error)
 }
