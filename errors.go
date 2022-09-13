56202fe852ab790587cef0f1b2f15b722c91bc7f
seoyhaein <seoy@bio-caleb.com>
2022-09-13 20:03:56 +0900

    commit by seoy.

--- errors.go
+++ errors.go
@@ -6,6 +6,7 @@ import (
 
 // https://github.com/sirupsen/logrus
 // https://blog.advenoh.pe.kr/go/Go%EC%97%90%EC%84%9C%EC%9D%98-%EB%A1%9C%EA%B7%B8%EA%B9%85-Logging-in-Go/
+// TODO config 에 있는 것과 통일하자.
 var log = logrus.New()
 
 type (
